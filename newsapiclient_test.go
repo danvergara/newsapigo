f713d475763442594ec6a6dc73e0fdb670b8aba5
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-07-04 17:09:54 -0500

    test(client-test): improve the documentation in client test file

--- newsapiclient_test.go
+++ newsapiclient_test.go
@@ -13,32 +13,39 @@ import (
 
 func TestFailApiKey(t *testing.T) {
 	sv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Fake Error Response
+		// When the API Key is missing or not provided
 		errorResponse := Response{
 			Status:  "error",
 			Code:    "apiKeyMissing",
 			Message: "Your API key is missing. Append this to the URL with the apiKey param, or use the x-api-key HTTP header.",
 		}
 
-		encoder := json.NewEncoder(w)
-
+		// If the Api Key is not present in headers, raise an error
 		if r.Header.Get("X-Api-Key") == "" {
 			t.Error("API Key not provided")
 		}
 
+		encoder := json.NewEncoder(w)
 		encoder.Encode(errorResponse)
 	}))
 
 	defer sv.Close()
-
+	// Parse an URL from the test server
 	rawURL, _ := url.Parse(sv.URL)
+
+	// Create a a client with the test URL as baseURL
 	testClient := &http.Client{Timeout: time.Minute}
 	c := NewsClient{
 		httpClient: testClient,
 		baseURL:    rawURL,
 	}
-
+	// Query params object creation
 	queryParams := EverythingArgs{}
+
+	// Make the http request without api key
 	_, err := c.Everything(queryParams)
 
+	// The error shouldn't be nil
 	assert.NotNil(t, err)
 }
